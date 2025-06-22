77af3c1b6ca136261d1a82c22e71f8f2e0f2f4cd
AoDem0 <dsw52708@student.dsw.edu.pl>
2025-06-22 03:05:14 +0200

    hp slider

--- ZombieGame/Assets/homeManager.cs
+++ ZombieGame/Assets/homeManager.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class homeManager : MonoBehaviour
 {
     public int homeCurrentHP;
     public int homeMaxHP;
+    [SerializeField] private Slider slider;
+    void Start()
+    {
+        slider.value = (float)homeCurrentHP / homeMaxHP;
+    }
     public void takeDMGHome()
     {
         homeCurrentHP -= 10;
-        /*slider.value = (float)homeCurrentHP / homeMaxHP;
+        slider.value = (float)homeCurrentHP / homeMaxHP;
         if (homeCurrentHP <= 0)
         {
             EndGame();
-        }*/
+        }
+    }
+    private void EndGame()
+    {
+        SceneManager.LoadScene("death");
     }
 }
