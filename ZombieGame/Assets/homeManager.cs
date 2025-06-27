51e14f4edcd215da08437ad1cc6400a33e7198a4
AoDem0 <dsw52708@student.dsw.edu.pl>
2025-06-27 13:23:18 +0200

    last changes
    all upgrades and times between waves changed

--- ZombieGame/Assets/homeManager.cs
+++ ZombieGame/Assets/homeManager.cs
@@ -22,6 +22,6 @@ public class homeManager : MonoBehaviour
     }
     private void EndGame()
     {
-        SceneManager.LoadScene("death");
+        SceneManager.LoadScene("death2");
     }
 }
---
+++ ZombieGame/Assets/unlockMouse.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class unlockMouse : MonoBehaviour
+{
+      void Start()
+    {
+
+        //wył widoczności kursora i lock kursora
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+}
